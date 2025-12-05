cb7f730cb8ed7dd784b90552a3609d738aed03ed
Zachary Rabinowitz <zachrabinowitz@Zacharys-MacBook-Pro.local>
2025-12-05 13:17:22 -0500

    Fix deployment

--- app.py
+++ app.py
@@ -2471,8 +2471,7 @@ def sync_toast_data():
 # ----------------------
 # Run it
 # ----------------------
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
